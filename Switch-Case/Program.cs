8d73528502cb59afac186d724da2c2fbda0d2ac6
Muhammed Ipekliogullari <muhammednecati20@gmail.com>
2023-02-28 17:51:20 +0300

    Add project files.

---
+++ Switch-Case/Program.cs
@@ -0,0 +1,49 @@
+﻿int month = DateTime.Now.Month;
+
+// Expression
+switch (month)
+{
+    case 1:
+        Console.WriteLine("ocak ayındasınız!");
+        break;
+    case 2:
+        Console.WriteLine("şubat ayındasınız!");
+        break; 
+    case 3:
+        Console.WriteLine("mart ayındasınız!");
+        break;
+    case 4:
+        Console.WriteLine("nisan ayındasınız!");
+        break;
+
+
+    default:
+        Console.WriteLine("Yanlış veri girilidi!");
+    break;
+}
+
+switch (month)
+{
+    case 12:
+    case 1:
+    case 2:
+        Console.WriteLine("Kış Ayındasınız!");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("İlk bahardasınız");
+        break;
+    case 9:
+        case 10:
+        case 11:
+        Console.WriteLine("Yaz ayındasınız");
+        break;
+    default:
+        break;
+
+
+
+
+
+}
